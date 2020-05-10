dccfc65bad641f520fd1fbc305e26ccc4ae43f59
Aécio Costa <aeciovc@gmail.com>
2020-05-10 11:03:12 +0300

    Filter implementation for name/category

---
+++ product/filters.py
@@ -0,0 +1,12 @@
+from django_filters import rest_framework as filters
+
+from product.models import Product
+
+class ProductFilter(filters.FilterSet):
+
+    class Meta:
+        model = Product
+        fields = {
+            'name': ['istartswith'],
+            'category': ['exact']
+        }
